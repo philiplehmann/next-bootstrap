50ef885fe041fec28b85bc21d033a0bbe796e2b9
Philip Lehmann <philiplehmann@gmail.com>
2021-07-16 16:21:00 +0200

    correct prisma event catching

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,4 +1,19 @@
 import { PrismaClient } from '@prisma/client'
+
+export type QueryEvent = {
+  timestamp: Date
+  query: string // Query sent to the database
+  params: string // Query parameters
+  duration: number // Time elapsed (in milliseconds) between client issuing query and database responding - not only time taken to run query
+  target: string
+}
+
+export type LogEvent = {
+  timestamp: Date
+  message: string
+  target: string
+}
+
 export const prisma = new PrismaClient({
   log: [
     {
@@ -6,20 +21,37 @@ export const prisma = new PrismaClient({
       level: 'query'
     },
     {
-      emit: 'stdout',
+      emit: 'event',
       level: 'error'
     },
     {
-      emit: 'stdout',
+      emit: 'event',
       level: 'info'
     },
     {
-      emit: 'stdout',
+      emit: 'event',
       level: 'warn'
     }
   ]
 })
-prisma.$on('query', (e) => {
-  console.log(`Query: ${e.query}`)
-  console.log(`Duration: ${e.duration}ms`)
+prisma.$on('query', (event: QueryEvent) => {
+  // eslint-disable-next-line
+  console.info(`Query: ${event.query}`)
+  // eslint-disable-next-line
+  console.info(`Duration: ${event.duration}ms`)
+})
+
+prisma.$on('warn', (event: LogEvent) => {
+  // eslint-disable-next-line
+  console.warn(event)
+})
+
+prisma.$on('info', (event: LogEvent) => {
+  // eslint-disable-next-line
+  console.info(event)
+})
+
+prisma.$on('error', (event: LogEvent) => {
+  // eslint-disable-next-line
+  console.error(event)
 })
